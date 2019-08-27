54c8ff54d2eb2e2a18c962f9ba580e97c7a6a54c
cameron.baker28 <cameron.baker28@600900J210DE404>
2019-08-27 16:52:09 +1000

    sgd

---
+++ PR Crisis/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GenerateInput input;
+    bool held, pressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            pressed = true;
+        }
+        else pressed = false;
+
+        if (Input.anyKey)
+        {
+            held = true;
+        }
+        else held = false;
+        for (int i = 0; i < input.sequence.Length; i++)
+        {
+            //input.sequence[i];
+        }
+        
+    }
+}
---
+++ PR Crisis/Assets/Scripts/GenerateInput.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GenerateInput : MonoBehaviour
+{
+    public Image[] sequence;
+    public Sprite[] types;
+    public Transform previousObject, firstPiece, lastPiece;
+    public GameObject empty;
+    public float distance;
+    bool first = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnSequence();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+        
+        
+    }
+
+    public void SpawnSequence()
+    {
+        sequence = new Image[Random.Range(4, 10)];
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            if (first)
+            {
+                sequence[i] = Instantiate(empty, new Vector3(previousObject.position.x, previousObject.position.y, previousObject.position.z), Quaternion.identity, this.transform).GetComponent<Image>();
+                first = false;
+                firstPiece = sequence[i].transform;
+                continue;
+            }
+            sequence[i] = Instantiate(empty, new Vector3(previousObject.position.x + distance, previousObject.position.y, previousObject.position.z), Quaternion.identity, this.transform).GetComponent<Image>();
+            previousObject = sequence[i].transform;
+
+        }
+        lastPiece = sequence[sequence.Length - 1].transform;
+        foreach (Image piece in sequence)
+        {
+            piece.sprite = types[Random.Range(0, 2)];
+            //Debug.Log(types[Random.Range(0, 2)]);
+        }
+        float offset = Vector3.Distance(firstPiece.position, lastPiece.position) / 2;
+        transform.position = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z);
+    }
+}
