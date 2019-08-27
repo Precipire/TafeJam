5c7198be86b086dc86b23ba520e847e746f05df0
Cameronte10 <cjbaker30@yahoo.com.au>
2019-08-28 09:50:44 +1000

    nothing

--- PR Crisis/Assets/Scripts/GameController.cs
+++ PR Crisis/Assets/Scripts/GameController.cs
@@ -6,6 +6,7 @@ public class GameController : MonoBehaviour
 {
     public GenerateInput input;
     bool held, pressed;
+    float fill = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        /*if (Input.anyKeyDown)
         {
             pressed = true;
         }
@@ -24,12 +25,79 @@ public class GameController : MonoBehaviour
         if (Input.anyKey)
         {
             held = true;
-        }
+        }*/
         else held = false;
         for (int i = 0; i < input.sequence.Length; i++)
         {
-            //input.sequence[i];
+            if(input.sequence[i].name.Equals("Press"))
+            {
+                waitForKeyPress()
+                if(pressed)
+                {
+                    //switch sprite to "completed sprite"
+                    pressed = false;
+                }
+            }
+            
+            if(input.sequence[i].name.Equals("Hold"))
+            {
+                if(!held)
+                {
+                    waitForKeyHold()
+                }
+                if(held)
+                {
+                    Hold();
+                }
+            }
         }
         
+    }
+    public IEnumerator waitForKeyHold()
+    {   
+        bool done = false;
+        while(!done) 
+        {
+            if(Input.anyKey)
+            {   
+                done = true; 
+            }
+            yield return null; 
+        }
+        pressed = true;
+    }
+    
+    public IEnumerator waitForKeyPress()
+    {   
+        bool done = false;
+        while(!done) 
+        {
+            if(Input.anyKeyDown)
+            {   
+                done = true; 
+            }
+            yield return null; 
+        }
+        held = true;
+    }
+    
+    public void Hold()
+    {
+        if(!Input.anyKey)
+        {
+            held = false;
+            return;
+        }
+        else
+        {
+            if(fill<0.8f)
+            {
+                fill += Time.deltaTime;
+            }
+            else
+            {
+                //switch sprite to "completed sprite"
+            }
+        }
     }
 }
